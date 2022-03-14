a2d0c13867ea4b461e73947686935c0510fbb49c
turtleprince <81336142+turtleprince@users.noreply.github.com>
2022-03-14 16:08:26 -0400

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,2 +1,12 @@
 function send(){
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
 }
+
+var  question_number = "<h4>" + number1 + "X"+ number2 +"<h4>";
+var  input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+var  ckeck_button = "<br><br><button class='btn btn-info' onclick='check()'> Check</button>";
+var  row = question_number + input_box + check_button ;
+document.getElementById("number1").value = "";
+document.getElementById("number2").value = "";
