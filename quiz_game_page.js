133d714e743b2d06c121ab9da5897146944e09a6
turtleprince <81336142+turtleprince@users.noreply.github.com>
2022-03-16 15:55:13 -0400

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -10,3 +10,35 @@ var  ckeck_button = "<br><br><button class='btn btn-info' onclick='check()'> Che
 var  row = question_number + input_box + check_button ;
 document.getElementById("number1").value = "";
 document.getElementById("number2").value = "";
+
+var question_turn = "player1";
+var answer_trun = "player2";
+
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer)
+    {
+        if(answer_trun == "player1")
+        {
+            update_player_score == player1_score +1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+        }
+        else
+        {
+            update_player_score == player2_score +1;
+            document.getElementById("player2_score").innerHTML = update_player2_score;
+        }
+    }
+
+    if(question_turn == "player1")
+    {
+        question_trun = "player2";
+        document.getElementById("player1_question").innerHTML = "Question Turn - " + player2_name;
+    }
+    else
+        {
+            question_turn = "player1";
+            document.getElementById("player1_question").innerHTML = "Question Turn - " + player1_name;
+        }
+    
+}
